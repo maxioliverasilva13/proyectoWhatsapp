3a53b8d82494ae2b381c24715d596fc9297d9220
Maximiliano Olivera <maximiliano.olivera@dualbootpartners.com>
2024-10-26 19:02:30 -0300

    fixes

--- deployCompanies.js
+++ deployCompanies.js
@@ -73,7 +73,7 @@ function deployApp() {
 
     execSync(`ssh -i private_key -o StrictHostKeyChecking=no root@${dropletIp} 'mkdir -p /path/on/droplet/app'`);
     execSync(`scp -i private_key -o StrictHostKeyChecking=no -r ./docker-compose-app.yml root@${dropletIp}:/path/on/droplet/app/docker-compose-app.yml`);
-    execSync(`ssh -i private_key root@${dropletIp} 'cd /path/on/droplet/app && docker-compose up -d'`);
+    execSync(`ssh -i private_key root@${dropletIp} 'cd /path/on/droplet/app && docker-compose -f docker-compose-app up -d'`);
 }
 
 (async () => {
