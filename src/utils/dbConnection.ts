20cc1017fffd6662aeb8e4e7fadb7365dde60bae
Maximiliano Olivera <maximiliano.olivera@dualbootpartners.com>
2024-10-30 11:06:43 -0300

    fixes

--- src/utils/dbConnection.ts
+++ src/utils/dbConnection.ts
@@ -12,6 +12,7 @@ export const handleGetConnectionValuesToCreateEmpresaDb = () => {
 };
 
 export const handleGetConnection = async () => {
+  console.log("here handleGetConnection")
   const env = process.env.SUBDOMAIN;
   const host = env === 'app' ? `${process.env.POSTGRES_GLOBAL_DB_HOST}` : `${env}-db`;
   return TypeOrmModule.forRoot({
@@ -33,6 +34,7 @@ export const handleGetConnection = async () => {
 };
 
 export const handleGetGlobalConnection = async () => {
+  console.log("here handleGetGlobalConnection")
   const globalConnection = new DataSource({
     type: 'postgres',
     host: `${process.env.POSTGRES_GLOBAL_DB_HOST}`,
