24b2d63998e20fd3c295e7cb08b4631857ae4983
Maximiliano Olivera <maximiliano.olivera@dualbootpartners.com>
2024-11-02 11:47:33 -0300

    fixes

--- deployCompanies.js
+++ deployCompanies.js
@@ -80,6 +80,10 @@ async function deployCompany(empresa) {
   await execSync(
     `ssh -i private_key -o StrictHostKeyChecking=no root@${dropletIp} 'mkdir -p /projects/${empresa?.db_name}'`,
   );
+  // remove old .env
+  await execSync(
+    `ssh -i private_key -o StrictHostKeyChecking=no root@${dropletIp} 'rm -f /projects/${empresa?.db_name}/.env'`
+  );
   await execSync(
     `scp -i private_key -o StrictHostKeyChecking=no -r .env.${empresa.db_name} root@${dropletIp}:/projects/${empresa?.db_name}/.env`,
   );
@@ -87,7 +91,7 @@ async function deployCompany(empresa) {
     `rsync -avz -e "ssh -i private_key -o StrictHostKeyChecking=no" --exclude='node_modules' ./ root@${dropletIp}:/projects/${empresa?.db_name}/`,
   );
   await execSync(
-    `ssh -i private_key root@${dropletIp} 'cd /projects/${empresa?.db_name} && docker-compose -f docker-compose.yml up -d --build'`,
+    `ssh -i private_key root@${dropletIp} 'cd /projects/${empresa?.db_name} && docker-compose -f docker-compose.yml up -d --build --remove-orphans'`,
   );
 }
 
