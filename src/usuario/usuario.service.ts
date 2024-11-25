177d6468cb5614a83ccedb1ce4238e8f7e0bd2f1
Rodrigo Olivera Silva <68871996+rocady21@users.noreply.github.com>
2024-11-25 16:28:58 -0300

    fix

--- src/usuario/usuario.service.ts
+++ src/usuario/usuario.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { UpdateUsuarioDto } from './dto/update-usuario.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Usuario } from './entities/usuario.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsuarioService {
+  constructor(
+    @InjectRepository(Usuario)
+    private usuarioRepository : Repository<Usuario>
+  ){}
+
   create(createUsuarioDto: CreateUsuarioDto) {
     return 'This action adds a new usuario';
   }
@@ -16,8 +24,16 @@ export class UsuarioService {
     return `This action returns a #${id} usuario`;
   }
 
-  update(id: number, updateUsuarioDto: UpdateUsuarioDto) {
-    return `This action updates a #${id} usuario`;
+  async update(id: number, updateUsuarioDto: UpdateUsuarioDto) {
+    const usuario = await this.usuarioRepository.findOneBy({ id });
+  
+    if (!usuario) {
+      throw new Error(`Usuario con ID ${id} no encontrado.`);
+    }
+  
+    const usuarioActualizado = this.usuarioRepository.merge(usuario, updateUsuarioDto);
+  
+    return this.usuarioRepository.save(usuarioActualizado);
   }
 
   remove(id: number) {
