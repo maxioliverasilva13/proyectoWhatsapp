5806e39c1e9bf113ab8378d2850215d23b3d4f19
Maximiliano Olivera <maximiliano.olivera@dualbootpartners.com>
2024-12-23 17:09:36 -0300

    fixes

--- src/utils/dbConnection.ts
+++ src/utils/dbConnection.ts
@@ -26,7 +26,7 @@ export const handleGetConnection = async () => {
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
-    ...(env === "app" && process.env.ENV !== 'dev' ? {
+    ...(process.env.ENV !== 'dev' ? {
       ssl: {
         rejectUnauthorized: false,
       },
