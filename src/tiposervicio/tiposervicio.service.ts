491e6b40c63aeb438b2408fb1c6fa500c7b0666c
Rocady21 <rodrigo.olivera@estudiantes.utec.edu.uy>
2025-01-20 19:23:19 -0300

    fix
    fix

--- src/tiposervicio/tiposervicio.service.ts
+++ src/tiposervicio/tiposervicio.service.ts
@@ -1,15 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateTiposervicioDto } from './dto/create-tiposervicio.dto';
 import { UpdateTiposervicioDto } from './dto/update-tiposervicio.dto';
+import { Tiposervicio } from './entities/tiposervicio.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class TiposervicioService {
-  create(createTiposervicioDto: CreateTiposervicioDto) {
+  constructor(
+    @InjectRepository(Tiposervicio)
+    private productoRepository: Repository<Tiposervicio>) { }
+
+  async create(createTiposervicioDto: CreateTiposervicioDto) {
     return 'This action adds a new tiposervicio';
   }
 
-  findAll() {
-    return `This action returns all tiposervicio`;
+  async findAll() {
+    try {
+      const allServices = await this.productoRepository.find()
+
+      return {
+        ok:true,
+        statusCode:200,
+        data: allServices
+      }
+
+    } catch (error) {
+      throw new BadRequestException({
+        ok: false,
+        statusCode: 400,
+        message: error?.message || 'Error al obtener los tipos de servicio',
+        error: 'Bad Request',
+      });
+    }
   }
 
   findOne(id: number) {
