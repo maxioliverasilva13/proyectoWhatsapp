4de1094e9b7bc3da7be67528eae86e1ab340ef9d
Rocady21 <rodrigo.olivera@estudiantes.utec.edu.uy>
2025-01-20 19:22:27 -0300

    get all types services

--- src/tiposervicio/tiposervicio.module.ts
+++ src/tiposervicio/tiposervicio.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TiposervicioService } from './tiposervicio.service';
 import { TiposervicioController } from './tiposervicio.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Tiposervicio } from './entities/tiposervicio.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Tiposervicio])],
   controllers: [TiposervicioController],
   providers: [TiposervicioService],
 })
