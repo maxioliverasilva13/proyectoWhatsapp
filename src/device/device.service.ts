9886c4afa2218c915c608c1e4bf93d4149c0ca43
Maximiliano Olivera <angelotunado02@gmail.com>
2025-04-05 10:48:58 -0300

    fix

--- src/device/device.service.ts
+++ src/device/device.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Usuario } from 'src/usuario/entities/usuario.entity';
@@ -16,13 +20,16 @@ export class DeviceService {
     private readonly usuarioRepository: Repository<Usuario>,
   ) {
     if (!admin.apps.length) {
-      admin.initializeApp({
-        credential: admin.credential.cert({
-          projectId: process.env.FIREBASE_PROJECT_ID,
-          clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-          privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-        }),
-      });
+      const subdomain = process.env.SUBDOMAIN;
+      if (subdomain === 'app') {
+        admin.initializeApp({
+          credential: admin.credential.cert({
+            projectId: process.env.FIREBASE_PROJECT_ID,
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+            privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+          }),
+        });
+      }
     }
     this.messaging = admin.messaging();
   }
@@ -55,24 +62,29 @@ export class DeviceService {
   }
 
   async sendNotificationUser(userId: number, title: string, desc: string) {
-    const usuario = await this.usuarioRepository.findOne({ where: { id: userId }, relations: ["dispositivo"] });
+    const usuario = await this.usuarioRepository.findOne({
+      where: { id: userId },
+      relations: ['dispositivo'],
+    });
 
-    console.log("usuario", usuario)
+    console.log('usuario', usuario);
     if (!usuario || !usuario.dispositivo || !usuario.dispositivo.fcmToken) {
-        throw new NotFoundException("User not device registered!");
+      throw new NotFoundException('User not device registered!');
     }
 
     const message = {
-        notification: { title: title, body: desc },
-        token: usuario.dispositivo.fcmToken,
+      notification: { title: title, body: desc },
+      token: usuario.dispositivo.fcmToken,
     };
 
     try {
-        await admin.messaging().send(message);
-        return { success: true, message: "Notification sended." };
+      await admin.messaging().send(message);
+      return { success: true, message: 'Notification sended.' };
     } catch (error) {
-        throw new InternalServerErrorException("Error sending notification", error.message);
+      throw new InternalServerErrorException(
+        'Error sending notification',
+        error.message,
+      );
     }
-}
-
+  }
 }
