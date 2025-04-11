4d98ee3c487e3e0caedbee8a8082c48cd47ae083
Maximiliano Olivera <angelotunado02@gmail.com>
2025-04-11 18:03:28 -0300

    fix

--- src/utils/greenApi.ts
+++ src/utils/greenApi.ts
@@ -7,7 +7,7 @@ export async function connectToGreenApi(): Promise < void> {
             apiTokenInstance: process.env.API_TOKEN_INSTANCE
         }));
         await restAPI.settings.setSettings({
-            webhookUrl: `https://2b5b2545de30.ngrok.app/webhooks`        
+            webhookUrl: `https://${process.env.VIRTUAL_HOST}/webhooks`
         });
         console.log('Conectado a Green API:');
     } catch(error) {
