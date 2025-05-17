f5738d4c30a86293afc50345b539ff5ffcf49203
Maximiliano Olivera <angelotunado02@gmail.com>
2025-05-17 17:44:17 -0300

    fix

--- src/utils/greenApi.ts
+++ src/utils/greenApi.ts
@@ -9,7 +9,7 @@ export async function connectToGreenApi(): Promise < void> {
         await restAPI.settings.setSettings({
             webhookUrl: `https://${process.env.VIRTUAL_HOST}/webhooks`
         });
-        console.log('Conectado a Green API:');
+        console.log('Conectado a Green APIii:');
     } catch(error) {
         console.error('Error al conectar con Green API:', JSON.stringify(error));
     }
