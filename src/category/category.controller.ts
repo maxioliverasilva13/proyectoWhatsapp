079fa19a088413e8cd4f8d4f06bb7be1430f7099
Rocady21 <rodrigo.olivera@estudiantes.utec.edu.uy>
2025-06-05 12:50:57 -0300

    fix

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -33,8 +33,8 @@ export class CategoryController {
   }
 
   @Delete(':id')
-  async deleteCategory(@Param('idCategory') idCategory: string) {
-    return this.categoryService.deleteCategory(parseInt(idCategory))
+  async deleteCategory(@Param('id') id: string) {
+    return this.categoryService.deleteCategory(parseInt(id))
   }
 
   @Patch(':id')
