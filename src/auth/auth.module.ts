0a67549dc13546235e8c93996e9e9af521451fa3
Maximiliano Olivera <68874048+maxioliverasilva13@users.noreply.github.com>
2025-06-06 18:54:07 -0300

    fix(token): change token expiration

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -16,7 +16,7 @@ import { EmailModuleResend } from 'src/emailServiceResend/email.module';
     PassportModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET_KEY,
-      signOptions: { expiresIn: '2h' },
+      signOptions: { expiresIn: '365d' }, // use refresh token
     }),
     EmailModule,
     EmailModuleResend
