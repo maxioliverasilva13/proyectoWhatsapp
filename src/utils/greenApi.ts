a4a245eddb92ba26d83ba14e21a12893e3e3eb9a
Rocady21 <rodrigo.olivera@estudiantes.utec.edu.uy>
2025-07-01 23:03:16 -0300

    fixes

--- src/utils/greenApi.ts
+++ src/utils/greenApi.ts
@@ -7,11 +7,10 @@ export async function connectToGreenApi(): Promise < void> {
             apiTokenInstance: process.env.API_TOKEN_INSTANCE
         }));
         await restAPI.settings.setSettings({
-            webhookUrl: `https://df7a-2800-a4-c1c3-b800-9154-99a6-1a5a-e0b9.ngrok-free.app/webhooks`
+            webhookUrl: `https://${process.env.VIRTUAL_HOST}/webhooks`
         });
         console.log('Conectado a Green APIii 4:');
     } catch(error) {
         console.error('Error al conectar con Green API:', JSON.stringify(error));
     }
 }
-
